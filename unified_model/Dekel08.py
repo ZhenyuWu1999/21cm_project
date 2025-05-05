d50f02fb8c315668311b309903e30495f035347d
Zhenyu Wu <zwu@cuillin.roe.ac.uk>
2025-05-05 17:57:39 +0100

    Dekel08.py: use ludlow16 concentration as default; Now the cooling from Eq24 and Eq25 agree with each other, no longer use Eq26 approx; use profile_corr, not C^3/(90*A0_C)

--- unified_model/Dekel08.py
+++ unified_model/Dekel08.py
@@ -6,6 +6,7 @@ from physical_constants import *
 import HaloProperties
 from Analytic_halo_profile import f_NFW, f_core, get_concentration
 import os
+import matplotlib.ticker as ticker
 concentration_model = 'ludlow16'
 
 def get_Dekel08_A(z):
@@ -32,16 +33,15 @@ def get_Velvirial(M, z):
     Vvir = V300 * 300
     return Vvir
 
-def get_concentration_Bullock01(M , z):
-    #M in Msun
-    M13 = M/1e13
-    return 9.0*M13**(-0.15)/(1+z)
 
 def get_T6_approx(M, z):
+    #M in Msun
     #Tvir in 10^6 K
-    M13 = M/1e13
-    A = get_Dekel08_A(z)
-    T6 = 2.33*M13**(2/3)*A**(-1)
+    # M13 = M/1e13
+    # A = get_Dekel08_A(z)
+    # T6 = 2.33*M13**(2/3)*A**(-1)
+    T = HaloProperties.Temperature_Virial_analytic(M, z)
+    T6 = T/1e6
     return T6
 
 def get_mean_metallicity_DB06(z):
@@ -96,7 +96,6 @@ def get_heating_Dekel08(M, z, fc):
     return: Edot_heating in erg/s
     '''
     Vvir_kms = get_Velvirial(M, z)
-    # concentration = get_concentration_Bullock01(M, z)
     #debug
     concentration = get_concentration(M, z, concentration_model)
 
@@ -109,9 +108,11 @@ def get_heating_Dekel08(M, z, fc):
     heating_rate_erg = heating_rate * 1e7 #convert to erg/s
     return heating_rate_erg
 
-def get_cooling_Dekel08(M, z, fg):
-    # concentration = get_concentration_Bullock01(M, z)
+#Directly use Dekel08 Eq25, always about 1.01*get_cooling_Dekel08()
+def get_cooling_Dekel08_Eq25(M, z, fg):
+    #M in Msun, return cooling rate in erg/s
     concentration = get_concentration(M, z, concentration_model)
+    c = concentration
     C9 = concentration/9.0
     A0_C = f_core(concentration)
     M13 = M/1e13
@@ -122,11 +123,56 @@ def get_cooling_Dekel08(M, z, fg):
     Vvir_kms = get_Velvirial(M, z)
     cooling_rate = 1/2*(fg*M*Msun)*(Vvir_kms*1e3)**2 \
                 *0.061*C9**3/A0_C*(fg/0.05)*M13**(-2/3)*Lambda23/A**2/Gyr
+    
+    #use actual integral of the density profile instead of the approximation
+    profile_correction_for_cooling = c**3*(c**2+5*c+10)/30/(c+1)**5 * c**3/3/f_core(c)**2
+    profile_correction_Dekel08 = c**3/(90*f_core(c))
+    cooling_rate = cooling_rate * profile_correction_for_cooling / profile_correction_Dekel08
+
     cooling_rate_erg = cooling_rate * 1e7 #convert to erg/s
     return cooling_rate_erg
 
+def get_cooling_Dekel08(M, z, fg, debug_output = False):
+    #M in Msun, return cooling rate in erg/s
+    c = get_concentration(M, z, concentration_model)
+    A = get_Dekel08_A(z)
+    profile_correction_for_cooling = c**3*(c**2+5*c+10)/30/(c+1)**5 * c**3/3/f_core(c)**2
+    T6 = get_T6_approx(M, z)
+    Z = get_mean_metallicity_DB06(z)
+    Lambda23 = 6.0*(Z/0.3)**0.7 * T6**(-1) + 0.2*T6**(1/2)  #Lambda in 1e-23 erg/s*cm^3
 
-def get_cooling_Dekel08_approx(M, z, fg):
+    #Method 1: (use Dekel08 Eq20 for q, always = 1.0079*Method 2)
+    # q = 7.7e-5 *Lambda23 * (fg/0.05) / A**3  #erg/s/g
+    # kg_to_g = 1e3
+    # cooling = fg*M*Msun*kg_to_g * q * profile_correction_for_cooling
+
+    #Method 2: direct calculation for q
+    chi = 0.528 #(0.88*0.6)
+    # chi = 0.52
+    rho_virial = 5.52e-28 * A**(-3) #g/cm^3
+    rho_virial_kgcm3 = rho_virial * 1e-3 #kg/cm^3
+    cooling = chi**2/(mu*mp)**2 *fg**2 * (1e-23*Lambda23) * (M*Msun) * rho_virial_kgcm3 * profile_correction_for_cooling
+
+    if (debug_output):
+        print(f'M = {M:.3e} Msun = {M*h_Hubble:.3e} Msun/h')
+        print("temperature = ", T6, "[1e6 K]") 
+
+        print('fg = ', fg)
+        print("rho_virial = ", rho_virial)
+        print('A = ', A)
+        print('c = ', c)
+        print('T6 = ', T6)
+        print('Lambda23 = ', Lambda23)
+        print('profile_correction_for_cooling = ', profile_correction_for_cooling)
+        print('cooling = ', cooling)
+
+    return cooling
+
+   
+
+'''
+def get_cooling_Dekel08_Eq26(M, z, fg):
+    #M in Msun
     #return cooling rate in erg/s
     M13 = M/1e13
     A = get_Dekel08_A(z)
@@ -137,9 +183,9 @@ def get_cooling_Dekel08_approx(M, z, fg):
     cooling_rate = 6.2e41 * (fg/0.05)**2 * M13 * Lambda23 *A**(-3)
     return cooling_rate
 
+
 def get_heating_cooling_ratio_Dekel08(M, z, fc, fg):
 
-    # concentration = get_concentration_Bullock01(M, z)
     concentration = get_concentration(M, z, concentration_model)
     A0_C = f_core(concentration)
     C9 = concentration/9.0
@@ -160,7 +206,7 @@ def get_heating_cooling_ratio_Dekel08(M, z, fc, fg):
 
     heating_cooling_ratio = 1.9*phi_hat_r*A0_C/C9**3 *fc/fg/(fg/0.05) *M13**(0.816667) /Lambda23*A**2/a**2.25
     return heating_cooling_ratio
-
+'''
 
 
 
@@ -184,18 +230,21 @@ def plot_heating_cooling_ratio_Dekel08():
         print('fg = ', fg)
         heating_rate = []
         cooling_rate = []
-        heating_cooling_ratio = []
+        # heating_cooling_ratio = []
         for lgM in lgM_list:
             M = 10**lgM
             heating_rate.append(get_heating_Dekel08(M, z, fc))
-            cooling_rate.append(get_cooling_Dekel08(M, z, fg))
-            heating_cooling_ratio.append(get_heating_cooling_ratio_Dekel08(M, z, fc, fg))
+            cooling_rate_M = get_cooling_Dekel08_Eq25(M, z, fg)
+            cooling_rate_M_test = get_cooling_Dekel08(M, z, fg, debug_output=False)
+            print("ratio between cooling rate method 1 and method 2 = ", cooling_rate_M/cooling_rate_M_test)
+            cooling_rate.append(cooling_rate_M_test)
+            # heating_cooling_ratio.append(get_heating_cooling_ratio_Dekel08(M, z, fc, fg))
         heating_rate = np.array(heating_rate)
         cooling_rate = np.array(cooling_rate)
         ratio = heating_rate/cooling_rate
-        heating_cooling_ratio = np.array(heating_cooling_ratio)
         All_results.append(ratio)
-        All_results_2.append(heating_cooling_ratio)
+        # heating_cooling_ratio = np.array(heating_cooling_ratio)
+        # All_results_2.append(heating_cooling_ratio)
     
     colors = ['blue', 'green', 'red']
     linestyles = ['--', '--', '--']
@@ -205,8 +254,21 @@ def plot_heating_cooling_ratio_Dekel08():
     for i in range(len(All_results)):
         result = All_results[i]
         ax.plot(lgM_list, np.log10(result), color=colors[i], linestyle=linestyles[i], label=labels[i])
-        result2 = All_results_2[i]
-        ax.plot(lgM_list, np.log10(result2), color=colors[i], linestyle='-')
+        # result2 = All_results_2[i]
+        # ax.plot(lgM_list, np.log10(result2), color=colors[i], linestyle=':')
+    
+    plt.xticks(np.arange(11, 16, 1))   # 11, 12, ..., 15
+    plt.yticks(np.arange(-3, 4, 1))    # -3, -2, ..., 3
+
+    # Minor ticks
+    ax.xaxis.set_minor_locator(ticker.MultipleLocator(0.2))
+    ax.yaxis.set_minor_locator(ticker.MultipleLocator(0.5))
+
+    # Optional: show grid for minor ticks if desired
+    ax.tick_params(which='minor', length=4, color='gray')
+    ax.tick_params(axis='both', which='both', direction='in', top=True, right=True)
+
+
     ax.axhline(np.log10(1), color='black', linestyle='--')
     ax.set_xlabel('lgM [Msun]', fontsize=14)
     ax.set_ylabel('lg(heating/cooling)', fontsize=14)
@@ -232,6 +294,6 @@ def plot_heating_cooling_ratio_Dekel08():
 
 
 if __name__ == "__main__":
-    compare_Dekel08()
-    # plot_heating_cooling_ratio_Dekel08()
+    # compare_Dekel08()
+    plot_heating_cooling_ratio_Dekel08()
    

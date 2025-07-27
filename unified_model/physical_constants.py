9b02bd1f8f1b311b57ea12a3a334cb0a715f5993
Zhenyu Wu <zwu@cuillin.roe.ac.uk>
2025-07-27 22:45:01 +0100

    physical_constants: use mu=1.22 for minihalo

--- unified_model/physical_constants.py
+++ unified_model/physical_constants.py
@@ -44,9 +44,9 @@ Zsun = 0.01295 # solar metallicity, grackle default
 #Zsun = 0.0127 # solar metallicity, according to TNG data specifications
 
 Tvir_crit = np.float64(1e4)
-mu_neutral = np.float64(1.23)  # mean molecular weight for neutral primordial gas
+mu_neutral = np.float64(1.22)  # mean molecular weight for neutral primordial gas
 mu = np.float64(0.6) # mean molecular weight for ionized gas
-mu_minihalo = np.float64(1.0) # mean molecular weight for gas cloud ~200K - 1e4K
+mu_minihalo = np.float64(1.22) # mean molecular weight for gas cloud ~200K - 1e4K
 hydrogen_mass_fraction = np.float64(0.75)  # hydrogen mass fraction in primordial gas
 
 rho_crit_z0_kgm3 = 3.0 * H0_s**2 / (8.0 * np.pi * G_grav)
@@ -72,5 +72,5 @@ if __name__ == "__main__":
     print("Ombh2:", Ombh2)
     print("Omch2:", Omch2)
     print(cosmo.ns)
-    # print(f"age of the universe: {cosmology.Cosmology.age(cosmo, z = 0.0)} billion years")
+    print(f"age of the universe: {cosmology.Cosmology.age(cosmo, z = 17)} billion years")
 
